e1dd0ec211e14b74b5d4c030a888edcf51957dac
arxisme <arsh11100arsh@gmail.com>
2024-08-23 16:14:06 +0530

    changed the migration file

--- database/migrations/2024_08_16_052313_create_activity_table.php
+++ database/migrations/2024_08_16_052313_create_activity_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
                 $table->id();
                 $table->string('type');
                 $table->text('description');
-                $table->string('attachment_path')->nullable();
                 $table->foreignIdFor(User::class);
                 $table->timestamps();
 
